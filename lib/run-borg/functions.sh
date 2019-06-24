e2bd364251edd6d8851df456484a16aea6c1e49f
AJ Jordan <alex@strugee.net>
2019-06-23 19:11:07 -0700

    Move a bunch of run-borg-custom(8) code into lib/
    This is so that it can be shared with run-borg(8).

---
+++ lib/run-borg/functions.sh
@@ -0,0 +1,71 @@
+# Intended to be sourced for library functions, not run directly
+
+# Automatically sets up the following variables:
+#  * $CHRONIC
+#  * $SOFTERR_STREAM
+#  * $SOFTERR_EXITCODE
+
+CHRONIC=false
+test $(ps -o comm= $PPID) = chronic && CHRONIC=true
+
+SOFTERR_STREAM=2
+$CHRONIC && SOFTERR_STREAM=1
+SOFTERR_EXITCODE=1
+$CHRONIC && SOFTERR_EXITCODE=0
+
+ensure_args() {
+	if [ $# = 0 ]; then
+		echo usage: $0 [-w] FILE... 1>&2
+		exit 1
+	fi
+}
+
+test_is_root() {
+	if [ $(id -u) != 0 ]; then
+		echo $(basename $0): need to be root >&2
+		exit 1
+	fi
+}
+
+lockfile_acquire() {
+	if ! dotlockfile -pr0 $DOTLOCK_ARGS /var/lock/run-borg.lock; then
+		echo $(basename $0): failed to acquire lockfile >&$SOFTERR_STREAM
+		exit $SOFTERR_EXITCODE
+	fi
+}
+
+lockfile_release() {
+	dotlockfile -u /var/lock/run-borg.lock
+}
+
+gc() {
+	mountpoint -q /media/borg-stage && umount -R /media/borg-stage
+	# For some reason there's a weird alternate empty dataset mount underneath everything that doesn't unmount the first time around
+	mountpoint -q /media/borg-stage && umount -R /media/borg-stage
+	zfs destroy -r rpool@borg-stage
+}
+
+gc_if_needed() {
+	# Garbage-collect old staging snapshots in case we crashed last time
+	zfs list -d1 rpool@borg-stage >/dev/null 2>&1 && gc
+}
+
+freeze_fs() {
+	# Freeze a view of the filesystem so Borg backups are atomic
+	zfs snapshot -r rpool@borg-stage
+	mkdir -p /media/borg-stage
+	zfs-mount-snapshots borg-stage /media/borg-stage
+}
+
+invoke_borg() {
+	borg create --progress --stats --compression auto,lzma,6 \
+	--exclude root/.gdfuse/default/cache \
+	--exclude root/.cache \
+	--exclude home/*/.cache \
+	--exclude var/cache \
+	--exclude var/backups \
+	--exclude var/tmp \
+	--exclude var/log \
+	/media/gdrive/borg::steevie-$(date +%F-%H:%M-%a-%s)-$BORG_TAG \
+	$@
+}

8f0b54d5a1ffc6f34f9c47089f6e735b50acb654
AJ Jordan <alex@strugee.net>
2020-08-04 02:48:39 -0700

    Add some logging code to the run-borg library

--- lib/run-borg/functions.sh
+++ lib/run-borg/functions.sh
@@ -13,6 +13,39 @@ $CHRONIC && SOFTERR_STREAM=1
 SOFTERR_EXITCODE=1
 $CHRONIC && SOFTERR_EXITCODE=0
 
+logging_cleanup() {
+	rm -f $STDOUT_FIFO $STDERR_FIFO
+}
+
+logging_init() {
+	# Note well: since we are sending stdout and stderr to different systemd-cat processes to get
+	# differing priorities for those, the output is not strictly ordered, though it will tend to be
+	# approximately ordered. See the description of --stderr-priority= in systemd-cat(1).
+
+	# IMPORTANT: also, the caller is expected to add logging_cleanup() to the EXIT trap.
+
+	STDOUT_FIFO=$(mktemp -u)
+	mkfifo -m 0600 $STDOUT_FIFO
+	trap "rm -f $STDOUT_FIFO" EXIT
+
+	STDERR_FIFO=$(mktemp -u)
+	mkfifo -m 0600 $STDERR_FIFO
+	trap "rm -f $STDOUT_FIFO; rm -f $STDERR_FIFO" EXIT
+
+	# Start the background output/log processing pipeline
+	# Can't use /dev/stdout for tee because | overwrites the inherited stdout, so we set up an
+	# fd 3 instead
+	exec 3<&1 # Duplicate our original stdout to fd 3
+	<$STDOUT_FIFO sh -c "tee /dev/fd/3 | systemd-cat -t $(basename $0)" &
+	exec 4<&2 # Use fd 4 for the original stderr
+	<$STDERR_FIFO sh -c "tee /dev/fd/4 | systemd-cat -t $(basename $0) -p warning" &
+
+	# Send stdout to it
+	exec 1>$STDOUT_FIFO
+	# Ditto stderr
+	exec 2>$STDERR_FIFO
+}
+
 ensure_args() {
 	ARGSTR="$1"
 	shift

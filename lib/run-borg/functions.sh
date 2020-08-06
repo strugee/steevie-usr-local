09566a08c41483d48dfcba0e98c4938014806210
AJ Jordan <alex@strugee.net>
2020-08-06 03:25:42 -0700

    Allow logging directly to journald in Borg libs

--- lib/run-borg/functions.sh
+++ lib/run-borg/functions.sh
@@ -13,37 +13,73 @@ $CHRONIC && SOFTERR_STREAM=1
 SOFTERR_EXITCODE=1
 $CHRONIC && SOFTERR_EXITCODE=0
 
-logging_cleanup() {
-	rm -f $STDOUT_FIFO $STDERR_FIFO
-}
-
-logging_init() {
+logging_init_channels() {
 	# Note well: since we are sending stdout and stderr to different systemd-cat processes to get
 	# differing priorities for those, the output is not strictly ordered, though it will tend to be
 	# approximately ordered. See the description of --stderr-priority= in systemd-cat(1).
 
-	# IMPORTANT: also, the caller is expected to add logging_cleanup() to the EXIT trap.
+	# Abort if we've already done setup
+	test -z ${INFO_DIRECTWRITE_FIFO+x} || return
 
 	STDOUT_FIFO=$(mktemp -u)
 	mkfifo -m 0600 $STDOUT_FIFO
-	trap "rm -f $STDOUT_FIFO" EXIT
 
 	STDERR_FIFO=$(mktemp -u)
 	mkfifo -m 0600 $STDERR_FIFO
-	trap "rm -f $STDOUT_FIFO; rm -f $STDERR_FIFO" EXIT
+
+	INFO_DIRECTWRITE_FIFO=$(mktemp -u)
+	mkfifo -m 0600 $INFO_DIRECTWRITE_FIFO
+
+	WARN_DIRECTWRITE_FIFO=$(mktemp -u)
+	mkfifo -m 0600 $WARN_DIRECTWRITE_FIFO
 
 	# Start the background output/log processing pipeline
-	# Can't use /dev/stdout for tee because | overwrites the inherited stdout, so we set up an
-	# fd 3 instead
-	exec 3<&1 # Duplicate our original stdout to fd 3
-	<$STDOUT_FIFO sh -c "tee /dev/fd/3 | systemd-cat -t $(basename $0)" &
-	exec 4<&2 # Use fd 4 for the original stderr
-	<$STDERR_FIFO sh -c "tee /dev/fd/4 | systemd-cat -t $(basename $0) -p warning" &
 
-	# Send stdout to it
+	# The trap is in the subshell to prevent a subtle race condition. If the subprocess was
+	# scheduled by the kernel before the parent process was, all was well because it would be
+	# able to open the FIFO. However, if the parent reached EOF, ran the cleanup code in the EXIT
+	# trap, and exited before the subprocess was scheduled, then by the time the subprocess
+	# opened the FIFO to be file descriptor 0, the FIFO would already have been cleaned up.
+	#
+	# Thus, we simply run the trap in the subprocess so that cleanup is tied to the logging channel
+	# itself, not the parent process. This is (arguably) a more logically consistent place to put
+	# this cleanup *and*, as a bonus, it means we do not need to require calling code to register
+	# a cleanup function as a part of their EXIT traps.
+	(trap "rm $INFO_DIRECTWRITE_FIFO" EXIT && <$INFO_DIRECTWRITE_FIFO systemd-cat -t $(basename $0)) &
+	(trap "rm $WARN_DIRECTWRITE_FIFO" EXIT && <$WARN_DIRECTWRITE_FIFO systemd-cat -t $(basename $0) -p warning) &
+
+	# Can't use /dev/stdout for tee because | overwrites the inherited stdout, so we set up an
+	# fd 3 instead. Same for stderr and fd 4.
+	exec 3<&1 # Duplicate our original stdout to fd 3
+	(trap "rm $STDOUT_FIFO" EXIT && <$STDOUT_FIFO tee /dev/fd/3 > $INFO_DIRECTWRITE_FIFO) &
+	exec 4<&2 # Use fd 4 for the original stderr
+	(trap "rm $STDERR_FIFO" EXIT && <$STDERR_FIFO tee /dev/fd/4 > $WARN_DIRECTWRITE_FIFO) &
+}
+
+logging_init() {
+	# IMPORTANT: you should read the comments for logging_init_channels()
+	logging_init_channels
+
+	# Send stdout to the logging channel
 	exec 1>$STDOUT_FIFO
 	# Ditto stderr
 	exec 2>$STDERR_FIFO
+}
+
+log_channel_info() {
+	cat > $INFO_DIRECTWRITE_FIFO
+}
+
+log_channel_warn() {
+	cat > $WARN_DIRECTWRITE_FIFO
+}
+
+log_info() {
+	echo "$@" | log_channel_info
+}
+
+log_warn() {
+	echo "$@" | log_channel_warn
 }
 
 ensure_args() {

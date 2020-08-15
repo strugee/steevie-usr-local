6d9689231b647f54fc39098bd9ad88c2433051a6
AJ Jordan <alex@strugee.net>
2020-08-14 17:08:57 -0700

    Give Borg a full 10 seconds to acquire repo locks

--- lib/run-borg/functions.sh
+++ lib/run-borg/functions.sh
@@ -14,7 +14,7 @@ SOFTERR_EXITCODE=1
 $CHRONIC && SOFTERR_EXITCODE=0
 
 # Just a constant
-BORG_STD_FLAGS='--compression auto,lzma,6'
+BORG_STD_FLAGS='--compression auto,lzma,6 --lock-wait 10'
 
 logging_init_channels() {
 	# Note well: since we are sending stdout and stderr to different systemd-cat processes to get

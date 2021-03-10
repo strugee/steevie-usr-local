9fbb7ac6ad5831f42d4db920d04a947ff887c598
AJ Jordan <alex@strugee.net>
2021-03-10 12:45:03 -0800

    Strip leading slashes when building Borg excludes

--- lib/run-borg/functions.sh
+++ lib/run-borg/functions.sh
@@ -72,7 +72,7 @@ get_homedir_caches() {
 invoke_borg() {
 	borg create $BORG_FLAGS --progress --stats $BORG_STD_FLAGS \
 	--exclude root/.gdfuse/default/cache \
-	$(for i in $(get_homedir_caches); do printf -- "--exclude $i "; done) \
+	$(for i in $(get_homedir_caches | sed 's;^/;;'); do printf -- "--exclude $i "; done) \
 	--exclude var/cache \
 	--exclude var/backups \
 	--exclude var/tmp \
